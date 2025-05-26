73449612811072764c977bc6af836e047598656a
albqvictor1508 <albq.victor@gmail.com>
2025-05-26 20:41:45 -0300

    wip: add zod setup and whatsapp web socket

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,31 @@
-import {fastify} from "fastify";
-import {Client} from "whatsapp-web.js"
+import { fastify } from "fastify";
+import { Client } from "whatsapp-web.js";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
 const whatsappClient = new Client({});
 
 whatsappClient.on("qr", (qr) => {
-    console.log("QR CODE RECEIVED", qr);
-})
+	console.log("QR CODE RECEIVED", qr);
+});
 
 whatsappClient.on("ready", () => {
-    console.log("Client is ready!");
-})
+	console.log("Client is ready!");
+});
 
-whatsappClient.on("message", msg => {
-    if(msg.body === "!ping") {
-        msg.reply("pong");
-    }
-})
+whatsappClient.on("message", (msg) => {
+	const body = msg.body.trim().toLowerCase();
+	if (msg.body === "!ping") {
+		msg.reply("pong");
+	}
+});
 
 whatsappClient.initialize();
