23d503cafbe0162e19621b1b5b3b3560734799f1
albqvictor1508 <albq.victor@gmail.com>
2025-05-27 09:55:16 -0300

    fix: add db setup for connection

---
+++ src/db/client.ts
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+import { env } from "../common/env";
+import chalk from "chalk";
+
+const client = new MongoClient(env.DB_URL);
+
+const connect = async () => {
+	try {
+		await client.connect();
+		console.error(chalk.greenBright("DB SUCCESSFULLY CONNECTED!"));
+		return client.db("zentto-chatbot");
+	} catch (error) {
+		console.error(chalk.red("ERROR TO CONNECT IN DB"));
+		throw error;
+	}
+};
+
+export const db = await connect();
