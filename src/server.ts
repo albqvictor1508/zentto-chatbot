30227423e76b5775a21aec7b910341059a59fcfc
albqvictor1508 <albq.victor@gmail.com>
2025-05-27 20:06:21 -0300

    wip: add a minimal care

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import qrcode from "qrcode-terminal";
 import { env } from "./common/env";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+const userStates = new Map();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -26,9 +27,34 @@ whatsappClient.on("ready", () => {
 });
 
 whatsappClient.on("message", (msg) => {
-	const body = msg.body.trim().toLowerCase();
+	const chatId = msg.from;
+	const body = msg.body.trim();
+	const userState = userStates.get(chatId);
 	if (body === "!ping") {
 		msg.reply("pong");
+	}
+
+	if (!userState) {
+		userStates.set(chatId, { step: 1, data: {} });
+		return msg.reply("Olá, Vamos fazer seu cadastro. Qual o seu nome?");
+	}
+
+	switch (userState.step) {
+		case 1: {
+			userState.data.name = body;
+			userState.step = 2;
+			return msg.reply("Legal, agora me diga sua cidade!");
+		}
+		case 2: {
+			userState.data.city = body;
+			userState.step = 3;
+			return msg.reply("Beleza! Qual serviço você deseja?");
+		}
+		case 3: {
+			userState.data.service = body;
+			userState.step = 4;
+			return msg.reply(`Cadastro finalizado Sr. ${userState.data.name}!`);
+		}
 	}
 });
 
