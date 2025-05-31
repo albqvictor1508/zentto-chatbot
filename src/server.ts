69e783ff2a227530e54f86dc37ed8fe42cf0e407
albqvictor1508 <albq.victor@gmail.com>
2025-05-30 22:02:45 -0300

    wip: changes on chatbot

--- src/server.ts
+++ src/server.ts
@@ -93,7 +93,7 @@ Aqui estão algumas opções para facilitar seu atendimento:
 		}
 		default: {
 			//todo: colocar o chatgpt pra ler essa porra e analisar se alguma funcionalidade do chat resolve esse problema dele, senão, manda pro atendente
-			return msg.reply("sai dai seu canalha");
+			return msg.reply("mensagem automática do bot ");
 		}
 	}
 });
