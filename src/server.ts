d965b9d95625c8bb0b66e7ccf16704570a63dc1b
albqvictor1508 <albq.victor@gmail.com>
2025-06-09 20:10:38 -0300

    wip: adding trycatch to ensure that whatsapp-web.js works

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ import { Block, type ChatData } from "./types/chat";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 const userStates = new Map<string, ChatData>();
-const formatCpf = (cpf) => {
+const formatCpf = (cpf: string) => {
 	return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
 };
 const sayGrace = (date: Date): string => {
@@ -66,40 +66,41 @@ Antes de começarmos, digite o CPF no qual está ligada ao plano de internet, e 
 
 	switch (userState.step) {
 		case 1: {
-			if (body === "1") {
-				userState.step++;
+			try {
+				if (body === "1") {
+					userState.step++;
+					return msg.reply(
+						"Quer fazer plano com nois paizão, R$ 89,90 por 2KB de internet!",
+					);
+				}
+				const cpfRegex = /^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$/;
+				if (!cpfRegex.test(body))
+					return msg.reply(
+						"CPF inválido, tente novamente ou digite *1* para realizar um novo cadastro!",
+					);
+				const cpfValidated = formatCpf(body);
+				const { data } = await axios.request({
+					method: "get",
+					url: "/cliente",
+					data: {
+						qtype: "cnpj_cpf",
+						query: cpfValidated,
+						oper: "=",
+						page: "1",
+						rp: 5,
+						sortname: "cliente.id",
+						sortorder: "desc",
+					},
+				});
+				console.log(data.registros ? data.registros.length : "não tem");
+				console.log("CPF VALIDATED: %s", cpfValidated);
+				//exemplo
+				if (!data.registros)
+					return msg.reply(
+						"Não existe nenhum cliente cadastrado com esse CPF, Envie um CPF novamente ou digite 1 para realizar cadastro",
+					);
+				userState.data.cpf = cpfValidated;
 				return msg.reply(`
-Quer fazer plano com nois paizão, R$ 89,90 por 2KB de internet!
-							`);
-			}
-			const cpfRegex = /^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$/;
-			if (!cpfRegex.test(body))
-				return msg.reply(`
-CPF inválido, tente novamente ou digite *1* para realizar um novo cadastro!
-				`);
-			const cpfValidated = formatCpf(body);
-			const { data } = await axios.request({
-				method: "get",
-				url: "/cliente",
-				data: {
-					qtype: "cnpj_cpf",
-					query: cpfValidated,
-					oper: "=",
-					page: "1",
-					rp: 5,
-					sortname: "cliente.id",
-					sortorder: "desc",
-				},
-			});
-			console.log(data.registros ? data.registros.length : "não tem");
-			console.log("CPF VALIDATED: %s", cpfValidated);
-			//exemplo
-			if (!data.registros)
-				return msg.reply(`
-Não existe nenhum cliente cadastrado com esse CPF, Envie um CPF novamente ou digite 1 para realizar cadastro
-			 			`);
-			userState.data.cpf = cpfValidated;
-			return msg.reply(`
 Olá, [NOME_DO_CLIENTE]! Como posso ajudar?
 
 1 - Analisar status financeiro.
@@ -108,16 +109,20 @@ Olá, [NOME_DO_CLIENTE]! Como posso ajudar?
 
 Digite o número da opção desejada.
 `);
+			} catch (error) {
+				console.error(error);
+				return msg.reply("Erro no bot");
+			}
 		}
 		case 2: {
 			if (body === "1") {
 				userState.data.block = Block.ONE;
 				userState.step++;
 				return msg.reply(`
-				BLOCO DE ANALISAR STATUS FINANCEIRO!
+BLOCO DE ANALISAR STATUS FINANCEIRO!
 
-				1 - Segunda via do boleto. 
-				2 - Confirmar pagamento.
+1 - Segunda via do boleto. 
+2 - Confirmar pagamento.
 					`);
 				// 1: Se tiver um só boleto, retorna esse boleto em PDF, se tiver mais de um, lista os boleto e pergunta qual ele quer pagar
 				// 2: Se tiver um só boleto, confirma de cara se foi pago, senão, lista os boletos e pergunta qual ele pagou
