0e027cf083e47aca375381a7f5245e8eb8479b15
albqvictor1508 <albq.victor@gmail.com>
2025-06-21 09:07:01 -0300

    chore: comment unused type, i can use then later

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,22 +1,24 @@
 export type ChatData = {
-	step: number;
-	data: {
-		name?: string;
-		cpf?: string;
-		phone?: string;
-		block?: Block;
-		id?: string;
-	};
+  step: number;
+  data: {
+    name?: string;
+    cpf?: string;
+    phone?: string;
+    block?: Block;
+    id?: string;
+  };
 };
 
+/*
 export type DefaultQuery = {
-	page: string;
-	total: string;
-	registros: Array<{}>;
+  page: string;
+  total: string;
+  registros: Array<{}>;
 };
+*/
 
 export enum Block {
-	ONE = "financial status",
-	TWO = "internet status",
-	THREE = "talk to attendat",
+  ONE = "financial status",
+  TWO = "internet status",
+  THREE = "talk to attendat",
 }
