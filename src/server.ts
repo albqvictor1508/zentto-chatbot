7e36e6925aa70a63334f020aeb21e14a50ab4ac5
albqvictor1508 <albq.victor@gmail.com>
2025-06-21 08:48:57 -0300

    chore: merged

--- src/server.ts
+++ src/server.ts
@@ -3,22 +3,23 @@ import { Client, LocalAuth } from "whatsapp-web.js";
 import chalk from "chalk";
 import axios from "./db/axios";
 import {
-	serializerCompiler,
-	validatorCompiler,
-	type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import qrcode from "qrcode-terminal";
 import { Block, type ChatData } from "./types/chat";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 const userStates = new Map<string, ChatData>();
+
 const formatCpf = (cpf: string) => {
-	return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
 };
 const sayGrace = (date: Date): string => {
-	const hour = date.getHours();
-	if (hour >= 6 || hour < 12) return "Bom dia!";
-	return "Boa tarde!";
+  const hour = date.getHours();
+  if (hour >= 6 || hour < 12) return "Bom dia!";
+  return "Boa tarde!";
 };
 
 app.setSerializerCompiler(serializerCompiler);
@@ -27,80 +28,80 @@ app.setValidatorCompiler(validatorCompiler);
 const whatsappClient = new Client({ authStrategy: new LocalAuth() });
 
 whatsappClient.on("qr", (qr) => {
-	try {
-		console.log("QR CODE RECEIVED", qr);
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
-	qrcode.generate(qr, { small: true });
+  try {
+    console.log("QR CODE RECEIVED", qr);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+  qrcode.generate(qr, { small: true });
 });
 
 whatsappClient.on("ready", async () => {
-	try {
-		console.log("Client is connected!");
-	} catch (error) {
-		console.log(error);
-		throw error;
-	}
+  try {
+    console.log("Client is connected!");
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 });
 whatsappClient.on("message", async (msg) => {
-	const chatId = msg.from;
-	const body = msg.body.trim();
-	if (msg.from.includes("@g.us") || msg.from === "status@broadcast") return;
+  const chatId = msg.from;
+  const body = msg.body.trim();
+  if (msg.from.includes("@g.us") || msg.from === "status@broadcast") return;
 
-	if (body === "!ping") {
-		return msg.reply("pong");
-	}
+  if (body === "!ping") {
+    return msg.reply("pong");
+  }
 
-	if (body === "!care") {
-		userStates.set(chatId, { step: 1, data: {} });
-		return msg.reply(`${sayGrace(new Date())} 👋, Sou o *Zentto*, seu assistente virtual! Vamos resolver o que você precisa rapidinho. Como posso ajudar?
+  if (body === "!care") {
+    userStates.set(chatId, { step: 1, data: {} });
+    return msg.reply(`${sayGrace(new Date())} 👋, Sou o *Zentto*, seu assistente virtual! Vamos resolver o que você precisa rapidinho. Como posso ajudar?
 
 Antes de começarmos, digite o CPF no qual está ligada ao plano de internet, e se ainda não é um cliente, digite 1
 		`);
-	}
+  }
 
-	const userState = userStates.get(chatId);
-	if (!userState) return;
+  const userState = userStates.get(chatId);
+  if (!userState) return;
 
-	switch (userState.step) {
-		case 1: {
-			try {
-				if (body === "1") {
-					userState.step++;
-					return msg.reply(
-						"Quer fazer plano com nois paizão, R$ 89,90 por 2KB de internet!",
-					);
-				}
-				const cpfRegex = /^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$/;
-				if (!cpfRegex.test(body))
-					return msg.reply(
-						"CPF inválido, tente novamente ou digite *1* para realizar um novo cadastro!",
-					);
-				const cpfValidated = formatCpf(body);
-				const { data } = await axios.request({
-					method: "get",
-					url: "/cliente",
-					data: {
-						qtype: "cnpj_cpf",
-						query: cpfValidated,
-						oper: "=",
-						page: "1",
-						rp: 5,
-						sortname: "cliente.id",
-						sortorder: "desc",
-					},
-				});
-				console.log(data.registros ? data.registros.length : "não tem");
-				console.log("CPF VALIDATED: %s", cpfValidated);
-				//exemplo
-				if (!data.registros)
-					return msg.reply(
-						"Não existe nenhum cliente cadastrado com esse CPF, Envie um CPF novamente ou digite 1 para realizar cadastro",
-					);
-				userState.data.cpf = cpfValidated;
-				return msg.reply(`
+  switch (userState.step) {
+    case 1: {
+      try {
+        if (body === "1") {
+          userState.step++;
+          return msg.reply(
+            "Quer fazer plano com nois paizão, R$ 89,90 por 2KB de internet!",
+          );
+        }
+        const cpfRegex = /^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$/;
+        if (!cpfRegex.test(body))
+          return msg.reply(
+            "CPF inválido, tente novamente ou digite *1* para realizar um novo cadastro!",
+          );
+        const cpfValidated = formatCpf(body);
+        const { data } = await axios.request({
+          method: "get",
+          url: "/cliente",
+          data: {
+            qtype: "cnpj_cpf",
+            query: cpfValidated,
+            oper: "=",
+            page: "1",
+            rp: 5,
+            sortname: "cliente.id",
+            sortorder: "desc",
+          },
+        });
+        console.log(data.registros ? data.registros.length : "não tem");
+        console.log("CPF VALIDATED: %s", cpfValidated);
+        //exemplo
+        if (!data.registros)
+          return msg.reply(
+            "Não existe nenhum cliente cadastrado com esse CPF, Envie um CPF novamente ou digite 1 para realizar cadastro",
+          );
+        userState.data.cpf = cpfValidated;
+        return msg.reply(`
 Olá, [NOME_DO_CLIENTE]! Como posso ajudar?
 
 1 - Analisar status financeiro.
@@ -109,93 +110,93 @@ Olá, [NOME_DO_CLIENTE]! Como posso ajudar?
 
 Digite o número da opção desejada.
 `);
-			} catch (error) {
-				console.error(error);
-				return msg.reply("Erro no bot");
-			}
-		}
-		case 2: {
-			if (body === "1") {
-				userState.data.block = Block.ONE;
-				userState.step++;
-				return msg.reply(`
+      } catch (error) {
+        console.error(error);
+        return msg.reply("Erro no bot");
+      }
+    }
+    case 2: {
+      if (body === "1") {
+        userState.data.block = Block.ONE;
+        userState.step++;
+        return msg.reply(`
 BLOCO DE ANALISAR STATUS FINANCEIRO!
 
 1 - Segunda via do boleto. 
 2 - Confirmar pagamento.
 					`);
-				// 1: Se tiver um só boleto, retorna esse boleto em PDF, se tiver mais de um, lista os boleto e pergunta qual ele quer pagar
-				// 2: Se tiver um só boleto, confirma de cara se foi pago, senão, lista os boletos e pergunta qual ele pagou
-				//e nois dois, ter um tratamento caso não haja nenhum boleto em aberto
-			}
-			if (body === "2") {
-				userState.step++;
-				userState.data.block = Block.TWO;
-				//simples, só preciso saber como posso fazer essa query pra api do ixc
-				return msg.reply("Bloco de ver o status da internet");
-			}
+        // 1: Se tiver um só boleto, retorna esse boleto em PDF, se tiver mais de um, lista os boleto e pergunta qual ele quer pagar
+        // 2: Se tiver um só boleto, confirma de cara se foi pago, senão, lista os boletos e pergunta qual ele pagou
+        //e nois dois, ter um tratamento caso não haja nenhum boleto em aberto
+      }
+      if (body === "2") {
+        userState.step++;
+        userState.data.block = Block.TWO;
+        //simples, só preciso saber como posso fazer essa query pra api do ixc
+        return msg.reply("Bloco de ver o status da internet");
+      }
 
-			userState.data.block = Block.THREE;
-			userState.step++;
-			if (body === "3") {
-				return msg.reply("Bloco de falar com o atendente");
-			}
-			return msg.reply("É nois cara, vou te passar pro atendimento");
-		}
-		case 3: {
-			userState.step++;
-			if (userState.data.block === Block.ONE) {
-				if (body === "1") {
-					const { data: getBilletList } = await axios.request({
-						method: "get",
-						url: "/fn_areceber",
-						data: {
-							qtype: "fn_areceber.id_cliente",
-							query: "19",
-							oper: "=",
-							rp: "200",
-							sortname: "asc",
-							grid_param: "",
-						},
-					});
-					if (getBilletList.registros.length < 1) return;
-					const { data: getBilletArchive } = await axios.request({
-						method: "get",
-						url: "/get_boleto",
-						data: {
-							boletos: "",
-							juro: "N",
-							multa: "N",
-							atualiza_boleto: "arquivo",
-							base64: "S",
-						},
-					});
-					console.log(getBilletArchive);
-					return msg.reply("Lógica de segunda via do boleto");
-				}
-				if (body === "2") {
-					return msg.reply("Lógica de confirmar pagamento");
-				}
-			}
+      userState.data.block = Block.THREE;
+      userState.step++;
+      if (body === "3") {
+        return msg.reply("Bloco de falar com o atendente");
+      }
+      return msg.reply("É nois cara, vou te passar pro atendimento");
+    }
+    case 3: {
+      userState.step++;
+      if (userState.data.block === Block.ONE) {
+        if (body === "1") {
+          const { data: getBilletList } = await axios.request({
+            method: "get",
+            url: "/fn_areceber",
+            data: {
+              qtype: "fn_areceber.id_cliente",
+              query: "19",
+              oper: "=",
+              rp: "200",
+              sortname: "asc",
+              grid_param: "",
+            },
+          });
+          if (getBilletList.registros.length < 1) return;
+          const { data: getBilletArchive } = await axios.request({
+            method: "get",
+            url: "/get_boleto",
+            data: {
+              boletos: "",
+              juro: "N",
+              multa: "N",
+              atualiza_boleto: "arquivo",
+              base64: "S",
+            },
+          });
+          console.log(getBilletArchive);
+          return msg.reply("Lógica de segunda via do boleto");
+        }
+        if (body === "2") {
+          return msg.reply("Lógica de confirmar pagamento");
+        }
+      }
 
-			if (userState.data.block === Block.TWO) {
-				return msg.reply("Lógica de verificar status da internet");
-			}
+      if (userState.data.block === Block.TWO) {
+        return msg.reply("Lógica de verificar status da internet");
+      }
 
-			if (userState.data.block === Block.THREE) {
-				return msg.reply("Lógica de chamar atendente");
-			}
-			return "";
-		}
-	}
+      if (userState.data.block === Block.THREE) {
+        return msg.reply("Lógica de chamar atendente");
+      }
+      return "";
+    }
+  }
 });
 
 whatsappClient.on("disconnected", (reason) => {
-	console.log(chalk.yellow("WhatsApp Client disconnected:"), reason);
+  console.log(chalk.yellow("WhatsApp Client disconnected:"), reason);
 });
 
 whatsappClient.on("auth_failure", (message) => {
-	console.error(chalk.red("Authentication failed:"), message);
+  console.error(chalk.red("Authentication failed:"), message);
 });
 
 whatsappClient.initialize();
